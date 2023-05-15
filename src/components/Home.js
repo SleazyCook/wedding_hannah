23acb0ca150c6ea10aecbe422d83cc1ed02c6c5f
Andrew Cook <andrewjcook93@gmail.com>
2023-05-14 19:59:02 -0500

    add header text and animated button

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,19 @@ function Home() {
   return (
     <div>
 
-      Home
+      <header className="heading">
+
+        <span className="heading__greeting">Something went horribly wrong</span>
+
+        <span className="heading__couple">Caitlan & Hannah</span>
+
+        <span className="heading__action">are getting married on</span>
+
+        <span className="heading__date">July XX, 2023</span>
+
+        <a className="button" href="#" targer="_blank">R.S.V.P.</a>
+
+      </header>
 
     </div>
   )
